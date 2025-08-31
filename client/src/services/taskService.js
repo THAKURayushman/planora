92d45f457b148300a17ecd517ae47a74d539f6c1
Ayushman Thakur <thakurayushman333@gmail.com>
2025-08-31 20:42:17 +0530

    minor fix

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `${VITE_API_URL}`; // backend base URL
+const API_URL = `${import.meta.env.VITE_API_URL}`; // backend base URL
 
 // Get all tasks
 export const getTasks = async () => {
